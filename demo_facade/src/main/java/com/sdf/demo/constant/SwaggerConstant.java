b71a2abb454755a3b16f0714d23aeabadf9fe4d9
shudingfeng <243178208@qq.com>
2020-05-20 16:37:01 +0800

    2

---
+++ demo_facade/src/main/java/com/sdf/demo/constant/SwaggerConstant.java
@@ -0,0 +1,22 @@
+package com.sdf.demo.constant;
+
+/**
+ * @ClassName SwaggerConstant
+ * @Description: TODO
+ * @Author ShuDingfeng
+ * @createDate 2020-04-21-18:16
+ * @Version 1.0
+ **/
+
+public interface SwaggerConstant {
+
+    /**
+     * ??????
+     */
+    public static final String SWAGGER_NAME = "sdf";
+
+    /**
+     * ??????
+     */
+    public static final String SWAGGER_CODE = "SDF666";
+}
